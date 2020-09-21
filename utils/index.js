54c1d643242882949413e5ad2355058c8f3e8ba2
baimingxuan <354372738@qq.com>
2020-09-21 15:22:04 +0800

    token解析方法修改

--- utils/index.js
+++ utils/index.js
@@ -3,8 +3,8 @@ const { SECRET_KEY } = require('./constant')
 
 function decoded(req) {
   let token = req.get('Authorization')
-  if (token.indexOf('Bearer') === 0) {
-    token = token.replace('Bearer', '')
+  if (token.indexOf('Bearer ') === 0) {
+    token = token.replace('Bearer ', '')
   }
   return jwt.verify(token, SECRET_KEY)
 }
