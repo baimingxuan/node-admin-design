8c77b57b3d2f3e126cd2a10ce43aeb784589fa2a
baimingxuan <354372738@qq.com>
2022-03-12 23:47:13 +0800

    feat: log4j.js encapsulation

--- utils/log4j.js
+++ utils/log4j.js
@@ -12,3 +12,62 @@ const levels = {
     'error': log4js.levels.ERROR,
     'fatal': log4js.levels.FATAL
 }
+
+log4js.configure({
+    appenders:{
+        console:{type:'console'},
+        info:{
+            type: 'file',
+            filename: 'logs/all-logs.log'
+        },
+        error:{
+            type: 'dateFile',
+            filename: 'logs/log',
+            pattern: 'yyyy-MM-dd.log',
+            alwaysIncludePattern: true // 设置文件名称 filename + pattern 
+        }
+    },
+    categories:{
+        default:{
+            appenders:['console'],
+            level: 'debug'
+        },
+        error:{
+            appenders:['console'],
+            level: 'error'
+        }
+    }
+})
+
+/**
+ * 日志输出，level为debug
+ * @param {string} content
+ * 
+*/
+exports.debug = (content)=>{
+    const logger = log4js.getLogger();
+    logger.level = levels.debug;
+    logger.debug(content);
+}
+
+/**
+ * 日志输出，level为info
+ * @param {string} content
+ * 
+*/
+exports.info = (content)=>{
+    const logger = log4js.getLogger();
+    logger.level = levels.info;
+    logger.info(content);
+}
+
+/**
+ * 日志输出，level为error
+ * @param {string} content
+ * 
+*/
+exports.error = (content)=>{
+    const logger = log4js.getLogger();
+    logger.level = levels.error;
+    logger.error(content);
+}
