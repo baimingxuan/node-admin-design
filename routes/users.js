55c6a32e53148ce66b567bcc6153d9dec2da8832
baimingxuan <354372738@qq.com>
2022-03-13 23:55:07 +0800

    feat: develop user routes

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
+/**
+ * 用户管理模块
+*/
 const router = require('koa-router')()
+const User = require('../models/userSchema')
+const util = require('../utils/utils')
 
 router.prefix('/users')
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
-
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+router.post('/login', async (ctx)=>{
+  try {
+    const { userName, userPwd } = ctx.request.body;
+    const res = await User.findOne({
+      userName,
+      userPwd
+    })
+    if (res) {
+      ctx.body = util.success(res)
+    } else {
+      ctx.body = util.fail('账号或密码不正确')
+    }
+  } catch (error) {
+    ctx.body = util.fail(error)
+  }
 })
 
 module.exports = router
