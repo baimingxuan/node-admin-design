c233055ec3eb579973c1c39be1baf0b4a4e4148f
baimingxuan <354372738@qq.com>
2025-04-08 17:02:22 +0800

    feat Created Role model

---
+++ src/models/role.js
@@ -0,0 +1,50 @@
+import { DataTypes } from 'sequelize'
+import sequelize from '../config/db.js'
+
+const Role = sequelize.define(
+  'sys_role',
+  {
+    role_id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: '角色ID'
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+      comment: '角色名称'
+    },
+    level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      comment: '角色级别'
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      comment: '角色描述'
+    },
+    data_scope: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      comment: '数据权限'
+    },
+    create_by: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      comment: '创建者'
+    },
+    update_by: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      comment: '更新者'
+    }
+  },
+  {
+    tableName: 'sys_role',
+    comment: '角色表'
+  }
+)
+export default Role
