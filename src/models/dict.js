b01ecd4ec2cc15c4d8ec23288a7c9793e8f05181
baimingxuan <354372738@qq.com>
2025-04-09 15:51:16 +0800

    feat: Created Dict and DictDetail model

---
+++ src/models/dict.js
@@ -0,0 +1,47 @@
+export default (sequelize, DataTypes) => {
+  // 定义字典模型
+  const Dict = sequelize.define(
+    'sys_dict',
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: '字典ID'
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        comment: '字典名称'
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: '字典描述'
+      },
+      create_by: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        comment: '创建者'
+      },
+      update_by: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        comment: '更新者'
+      }
+    },
+    {
+      comment: '系统字典'
+    }
+  )
+
+  Dict.associate = function (models) {
+    // // 字典与字典详情一对多关系
+    Dict.hasMany(models.DictDetail, {
+      foreignKey: 'dict_id'
+    })
+  }
+
+  return Dict
+}
---
+++ src/models/dictDetail.js
@@ -0,0 +1,56 @@
+export default (sequelize, DataTypes) => {
+  // 定义字典详情模型
+  const DictDetail = sequelize.define(
+    'sys_dict_detail',
+    {
+      detail_id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+        comment: '字典详情ID'
+      },
+      dict_id: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        comment: '字典ID'
+      },
+      label: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        comment: '字典标签'
+      },
+      value: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        comment: '字典值'
+      },
+      dict_sort: {
+        type: DataTypes.INTEGER,
+        defaultValue: 999,
+        comment: '排序'
+      },
+      create_by: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        comment: '创建者'
+      },
+      update_by: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+        comment: '更新者'
+      }
+    },
+    {
+      comment: '系统字典详情'
+    }
+  )
+
+  DictDetail.associate = function (models) {
+    // 字典详情与字典多对一关系
+    DictDetail.belongsTo(models.Dict, {
+      foreignKey: 'dict_id'
+    })
+  }
+
+  return DictDetail
+}
